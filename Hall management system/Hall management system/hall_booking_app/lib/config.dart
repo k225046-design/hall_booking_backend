96697eb560e87e04810bf773b13245cbbeb3df3a
Mufrah_app. <k225046@nu.edu.pk>
2026-05-06 14:20:47 +0500

    updated backend url

--- Hall management system/Hall management system/hall_booking_app/lib/config.dart
+++ Hall management system/Hall management system/hall_booking_app/lib/config.dart
@@ -6,14 +6,5 @@ String get baseUrl {
     return configuredBaseUrl;
   }
 
-  if (kIsWeb) {
-    return 'http://127.0.0.1:5000';
-  }
-
-  switch (defaultTargetPlatform) {
-    case TargetPlatform.android:
-      return 'http://10.0.2.2:5000';
-    default:
-      return 'http://127.0.0.1:5000';
-  }
+  return 'https://hallbooking.pythonanywhere.com';
 }
